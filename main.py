898915ae9c65b995b4f568fb339e88e2dbfa8150
Tenembris <110739948+Tenembris@users.noreply.github.com>
2022-11-28 22:31:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import arxiv
 import PyPDF2
+import matplotlib.pyplot as plt
 search = arxiv.Search(
     query="IEEE 2021",
     max_results=30
@@ -32,11 +33,16 @@ def check_space(string):
 #     "\nZnaki bez spacji",e,
 #     "\nWyrazy",words
 #     )
+lhight = []
+lleft = []
+left = []
+height = []
 for x in range(30):
     file = open(f"D:\\PDFY\\paper{x}.pdf", 'rb')
     ReadPDF = PyPDF2.PdfFileReader(f"D:\\PDFY\\paper{x}.pdf")
     pages = ReadPDF.numPages
     print(f"pdf {x}",pages)
+
 
     Tword = 0
     WText = ""
@@ -48,12 +54,44 @@ for x in range(30):
     print(f"pdf {x}",Tword)
     string = WText
     print(check_space(string))
+    lhight.append(Tword)
+    lleft.append(x)
+    left.append(len(WText))
+    height.append(x)
     # print(text.split())
     # print(pagesObj)
     # print(WText)
 
     # print(WText)
     print(len(WText))
+    # print(lword)
+# tick_label = []
+plt.bar(lleft, lhight, tick_label = lleft, width=0.8, color = ["green", "red"])
+plt.xlabel('Artukuły')
+plt.ylabel('Ilość słów')
+plt.show()
+
+
+# print(left[1])
+
+# tick_label1=[]
+
+# plt.subplot(131)
+# plt.bar(height,left,tick_label=lleft,width=0.3)
+# plt.xlabel('')
+# plt.ylabel('ilość')
+# plt.suptitle('Ilość znaków w artykule')
+#
+#
+# plt.show()
+
+
+
+
+
+
+
+
 
 
 
